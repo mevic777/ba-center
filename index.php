b1fb356609a129e3a75630b808eee62d84bd3210
mevic777 <kovalimarius@gmail.com>
2022-10-11 07:16:52 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ba Center</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <h1 class="h3 mb-3 text-center" style="font-family: 'Playfair Display';">Quels sont les tarifs?</h1>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="title-procedure text-center p-0" style="background: #242444; color: white; font-family: 'Playfair Display'">
+                    <h4 class="p-2">Nettoyage de peau</h4>
+                </div>
+                <div class="price-procedure d-flex flex-column align-items-center">
+                    <h1 class="d-flex" style="font-family: 'Playfair Display'; font-size: 64px;">
+                        80
+                        <div class="d-flex flex-column" style="margin-top: 20px; margin-left: 10px;">
+                            <span style="font-size: 25.6px;">€</span>
+                            <span style="font-size: 15px; font-family: 'Roboto'; color: #999999;">/ 1 séance</span>
+                        </div>
+                    </h1>
+                </div>
+                <div class="details-procedure d-flex flex-column align-items-center" style="background: #242444; color: white;">
+                    <div class="dp-img mt-4">
+                        <img style="width: 60px;" src="icon.png" alt="icon">
+                    </div>
+                    <div class="dp-text p-1 mb-3">
+                        <p>30 minutes par séance</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="title-procedure text-center p-0" style="background: #242444; color: white; font-family: 'Playfair Display'">
+                    <h4 class="p-2">Nettoyage de la peau profond</h4>
+                </div>
+                <div class="price-procedure d-flex flex-column align-items-center">
+                    <h1 class="d-flex" style="font-family: 'Playfair Display'; font-size: 64px;">
+                        100 
+                        <div class="d-flex flex-column" style="margin-top: 20px; margin-left: 10px;">
+                            <span style="font-size: 25.6px;">€</span>
+                            <span style="font-size: 15px; font-family: 'Roboto'; color: #999999;">/ 6 séances</span>
+                        </div>
+                    </h1>
+                </div>
+                <div class="details-procedure d-flex flex-column align-items-center" style="background: #242444; color: white;">
+                    <div class="dp-img mt-4">
+                        <img style="width: 60px;" src="icon.png" alt="icon">
+                    </div>
+                    <div class="dp-text text-center">
+                        <p class="m-0" style="font-family: 'Roboto';">4 x 168,75 €</p>
+                        <p>30 minutes par séance</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="title-procedure text-center p-0" style="background: #242444; color: white; font-family: 'Playfair Display'">
+                    <h4 class="p-2">SOIN NETTOYAGE PROFOND</h4>
+                </div>
+                <div class="price-procedure d-flex flex-column align-items-center">
+                    <h1 class="d-flex" style="font-family: 'Playfair Display'; font-size: 64px;">
+                        130 
+                        <div class="d-flex flex-column" style="margin-top: 20px; margin-left: 10px;">
+                            <span style="font-size: 25.6px;">€</span>
+                            <span style="font-size: 15px; font-family: 'Roboto'; color: #999999;">/ 6 séances</span>
+                        </div>
+                    </h1>
+                </div>
+                <div class="details-procedure d-flex flex-column align-items-center" style="background: #242444; color: white;">
+                    <div class="dp-img mt-4">
+                        <img style="width: 60px;" src="icon.png" alt="icon">
+                    </div>
+                    <div class="dp-text text-center">
+                        <p class="m-0" style="font-family: 'Roboto';">4 x 236,25 €</p>
+                        <p>30 minutes par séance</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ product.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ba Center</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+</head>
+<body style="font-family: 'PT Serif'">
+    <div class="container">
+        <div class="card mt-5">
+            <div class="card-body">
+                
+                <div class="row">
+                    <div class="col-lg-5 col-md-5 col-sm-6">
+                        <div class="white-box text-center">
+                            <img src="seconds-cosmetics.png" style="width: 400px;">
+                        </div>
+                    </div>
+                    <div class="col-lg-7 col-md-7 col-sm-6">
+                        <h3 class="card-title">Cleanser shine control</h3>
+                        <h4 class="box-title mt-5">Product description</h4>
+                        <p>Lorem Ipsum available,but the majority have suffered alteration in some form,by injected humour,or randomised words which don't look even slightly believable.but the majority have suffered alteration in some form,by injected humour</p>
+                        <h2 class="mt-5">
+                            90 €
+                            <!-- <small class="text-success">(36%off)</small> -->
+                        </h2>
+                        <button class="btn btn-primary btn-rounded">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
